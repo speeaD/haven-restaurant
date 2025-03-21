508f416e1b35f069b3638c381178b6bea65aea9b
speeaD <sejiofor20@gmail.com>
2025-03-21 09:19:20 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Haven of Flavours - Haven',
+      title: 'Haven of Flavours - Haven of Flavours',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
